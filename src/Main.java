f4e3cb28c29225b84192b3cf4d5ad46fc79ea5c3
desalvtj <desalvtj@mail.uc.edu>
2024-09-21 11:16:12 -0400

    Final and Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        final double INTEREST_RATE = .17;
+        double interest = 0;
+        double creditCardBalance = 5000;
+
+        interest = creditCardBalance * INTEREST_RATE;
+        creditCardBalance = creditCardBalance + interest;
+
+        System.out.println("The first months interest is " + interest + ", for a total balance of " + creditCardBalance);
+
+        interest = creditCardBalance * INTEREST_RATE;
+        creditCardBalance = creditCardBalance + interest;
+
+        System.out.println("The second months interest is " + interest + ", for a total balance of " + creditCardBalance);
+
     }
 }
